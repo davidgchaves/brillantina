42a61cbc46cb6c9df918a9b8197f020453d200d7
David G Chaves <davidgchaves@gmail.com>
2015-08-06 07:46:42 +0200

    Add feature 'Homepage after asking user to log in welcomes user'
      - Leverages devise to create the user authentication:
        ✔ rails g devise user
      - Completely guided by Acceptance Example.
      - Comes after our spike from yesterday.

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,11 +1,21 @@
 require "rails_helper"
 
 feature "Homepage" do
-  scenario "loads by default" do
+  before do
+    User.create(email: 'paul@example.com', password: 'paul_at_example_dot_com')
+  end
+
+  scenario "after asking user to log in welcomes user" do
     visit "/"
 
+    expect(page).to have_content("Log in")
+
+    fill_in 'Email',    with: 'paul@example.com'
+    fill_in 'Password', with: 'paul_at_example_dot_com'
+    click_button 'Log in'
+
     within "h1" do
-      expect(page).to have_content("Brillantina!")
+      expect(page).to have_content("Welcome to Brillantina, paul@example.com!")
     end
   end
 end
