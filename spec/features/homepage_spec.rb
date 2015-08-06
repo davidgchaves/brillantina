86e6a265a0e9c5c39a1ce07d9e3b139edf885ce7
David G Chaves <davidgchaves@gmail.com>
2015-08-06 10:14:41 +0200

    Refactor Homepage Examples to better reveal intent
    Extracted common functionality to the 2 examples

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -2,36 +2,51 @@ require "rails_helper"
 
 feature "Homepage" do
   before do
-    User.create(email: 'paul@example.com', password: 'paul_at_example_dot_com')
+    User.create email: 'paul@example.com', password: 'paul_at_example_dot_com'
   end
 
   scenario "after asking user to log in welcomes user" do
-    visit "/"
+    visit_homepage
 
-    expect(page).to have_content("Log in")
+    expect(page).to have_content "Log in"
 
-    fill_in 'Email',    with: 'paul@example.com'
-    fill_in 'Password', with: 'paul_at_example_dot_com'
-    click_button 'Log in'
+    sign_in_with 'paul@example.com', 'paul_at_example_dot_com'
 
-    within "h1" do
-      expect(page).to have_content("Welcome to Brillantina, paul@example.com!")
-    end
+    expect_welcome_message_for 'paul@example.com'
   end
 
   scenario "after asking visitors to sign up welcomes new user" do
-    visit "/"
+    visit_homepage
     click_link "Sign up"
 
-    expect(page).to have_content("Sign up")
+    expect(page).to have_content "Sign up"
 
-    fill_in 'Email',                 with: 'new-user@example.com'
-    fill_in 'Password',              with: 'new-user-password'
-    fill_in 'Password confirmation', with: 'new-user-password'
-    click_button 'Sign up'
+    sign_up_with 'new-user@example.com', 'new-user-password'
 
-    within "h1" do
-      expect(page).to have_content("Welcome to Brillantina, new-user@example.com!")
-    end
+    expect_welcome_message_for 'new-user@example.com'
   end
+
+  private
+    def visit_homepage
+      visit "/"
+    end
+
+    def sign_in_with(email, password)
+      fill_in 'Email',    with: email
+      fill_in 'Password', with: password
+      click_button 'Log in'
+    end
+
+    def sign_up_with(email, password)
+      fill_in 'Email',                 with: 'new-user@example.com'
+      fill_in 'Password',              with: 'new-user-password'
+      fill_in 'Password confirmation', with: 'new-user-password'
+      click_button 'Sign up'
+    end
+
+    def expect_welcome_message_for(email)
+      within "h1" do
+        expect(page).to have_content "Welcome to Brillantina, #{email}!"
+      end
+    end
 end
